77e121a92843136ec64ff05c25a2516bbed5509d
Surgbc <github-action@actions-template-sync.noreply.github.com>
2023-12-25 11:33:48 +0000

    chore(template): merge template changes :up:

---
+++ meshes/botarmbots/24-007/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/3 point beige.hdr"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/ULMW-24-007-concept.mtl"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/ULMW-24-007-concept.obj"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/hood-24-007-concept.mtl"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/hood-24-007-concept.obj"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/innerLegBottom-24-007-concept.mtl"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/innerLegBottom-24-007-concept.obj"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/innerLegTop-24-007-concept.mtl"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/innerLegTop-24-007-concept.obj"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/outerLeg-24-007-concept.mtl"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/outerLeg-24-007-concept.obj"
+ wget "https://brianmechanisms.github.io/meshes/meshes/botarmbots/24-007/softbox.png"
